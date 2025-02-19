f66eab4a76eb5fca860bc37b71121e27f27d5e0d
Joan <79165234+Joangb85@users.noreply.github.com>
2025-02-19 20:10:46 +0000

    First DB implementation

---
+++ api/src/models.py
@@ -0,0 +1,34 @@
+
+from flask_sqlalchemy import SQLAlchemy
+from dataclasses import dataclass
+from sqlalchemy import ForeignKey
+from enum import Enum
+
+db = SQLAlchemy()
+
+    
+@dataclass     
+class Users(db.Model):
+    __tablename__ = 'Users'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID:int = db.Column(db.Integer, primary_key=True, unique=True)
+    first_name:str = db.Column(db.String(250), nullable=False)
+    last_name:str = db.Column(db.String(250), nullable=False)
+    email:str = db.Column(db.String(250), nullable=False, unique=True)
+    username:str = db.Column(db.String(250), nullable=False, unique=True)
+    password:str = db.Column(db.String(250), nullable=False)
+    def __repr__(self):
+        return '<Users %r>' % self.username
+
+@dataclass
+class Dashboard(db.Model):
+    __tablename__ = 'Films'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID:int = db.Column(db.Integer, primary_key=True, unique=True)
+    User_id:int = db.Column(db.Integer, ForeignKey(Users.ID), nullable=False)
+    level:str = db.Column(db.String(250), nullable=False)
+    time:int = db.Column(db.Integer, nullable=False)
+    def __repr__(self):
+        return '<Dashboard %r>' % self.dashboard
