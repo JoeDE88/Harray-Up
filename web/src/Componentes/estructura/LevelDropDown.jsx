f9c7c2f6be5af5597f0272feddc0d881e696a8af
Joe <joediaz88@hotmail.com>
2025-02-28 19:07:14 +0000

    creada funcion de llamada de niveles

--- web/src/Componentes/estructura/LevelDropDown.jsx
+++ web/src/Componentes/estructura/LevelDropDown.jsx
@@ -4,67 +4,78 @@ import Icon from '@mui/material/Icon';
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import { useTheme } from '@mui/material';
+import { useLevelContext } from '../../Contexts/LevelContext';
+import { NavLink as RouterLink } from 'react-router';
+import { root } from '../../calls';
+
 
 export default function LevelDropdown({ levels }) {
     // EN LUGAR DE PASAR onLevelSelect como prop, intento hacer fetch con esa función
+    const { level, setLevel } = useLevelContext();
+
     const onLevelSelect = () => {
-        fetch('https://vigilant-zebra-v6qvqrjr9jggcx67x-5000.app.github.dev/levels/1', {
+        fetch(`${root}/levels/1`, {
             method: "GET"
         })
-            .then((data) => { 
-                return data.json() })
-            .then((response) => console.log(response))
-}
-
-const [anchorEl, setAnchorEl] = React.useState(null);
-const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-};
-const handleClose = () => {
-    setAnchorEl(null);
-};
-
-const theme = useTheme();
-const menuItemStyles = {
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.secondary.main,
-    fontSize: '20px',
-    "&:hover": {
-        backgroundColor: theme.palette.tertiary.main
+            .then((data) => {
+                return data.json()
+            })
+            .then((response) => {
+                setLevel(response.content)
+                console.log(response.content)})
+            .catch((error) => console.error("Error fetching level:", error));
     }
-};
 
-return (
-    <div>
-        <Button
-            color="tertiary"
-            variant='contained'
-            style={{ fontSize: '20px', marginRight: '30px', marginLeft: '15px', marginTop: '5px', padding: '0px', paddingLeft: '10px' }}
-            aria-controls="simple-menu"
-            aria-haspopup="true"
-            onClick={handleClick}
-        >
-            Level 1
-            <Icon style={{ alignContent: 'center', marginLeft: '10px', fontSize: '40px' }} >arrow_drop_down</Icon>
-        </Button>
-        <Menu
-            color="tertiary"
-            id="simple-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-        >
-            {levels.map((level) => (
-                <MenuItem
-                    sx={menuItemStyles}
-                    variant='contained'
-                    onClick={() => { onLevelSelect(level); handleClose(); }}
-                >
-                    {level}
-                </MenuItem>
-            ))}
-        </Menu>
-    </div>
-);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const theme = useTheme();
+    const menuItemStyles = {
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.secondary.main,
+        fontSize: '20px',
+        "&:hover": {
+            backgroundColor: theme.palette.tertiary.main
+        }
+    };
+
+    return (
+        <div>
+            <Button
+                color="tertiary"
+                variant='contained'
+                style={{ fontSize: '20px', marginRight: '30px', marginLeft: '15px', marginTop: '5px', padding: '0px', paddingLeft: '10px' }}
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+            >
+                Level {level.id}
+                <Icon style={{ alignContent: 'center', marginLeft: '10px', fontSize: '40px' }} >arrow_drop_down</Icon>
+            </Button>
+            <Menu
+                color="tertiary"
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                {levels.map((level) => (
+                    <MenuItem
+                        key={level.id}
+                        sx={menuItemStyles}
+                        variant='contained'
+                        onClick={() => { onLevelSelect(); handleClose(); }}
+                    >
+                        {level}
+                    </MenuItem>
+                ))}
+            </Menu>
+        </div>
+    );
 }
