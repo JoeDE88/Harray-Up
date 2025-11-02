7bdf3221433fcd1608a0fe69dfce4d71db533f72
Joe Lizardo Diaz Echevarria <joediaz88@hotmail.com>
2025-11-02 22:32:13 +0000

    added login endpoint with access token

--- api/harrayApp/routers/users.py
+++ api/harrayApp/routers/users.py
@@ -1,11 +1,15 @@
+from datetime import timedelta
 from typing import Annotated
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, Path, status, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 
+
 from ..database.user_model import Users
 from ..database.database import SessionLocal
-from ..utils.security import bcrypt_context
+from ..utils.security import authenticate_user, bcrypt_context, create_access_token
 
 # DECLARING router FOR USERS
 router = APIRouter(
@@ -34,10 +38,13 @@ class CreateUserRequest(BaseModel):
     password: str
     role: str
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
 @router.get('/')
 async def get_users(db: db_dependency):
     users = db.query(Users).all()
-
     return users
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
@@ -50,7 +57,26 @@ async def create_user(db: db_dependency, user_request: CreateUserRequest):
         hashed_password = bcrypt_context.hash(user_request.password),
         role = user_request.role
     )
+    
     db.add(user_model)
     db.commit()
-
     return {'message':'User created successfully!'}
+
+@router.post('/login', response_model=Token)
+async def login_with_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Incorrect username or password.')
+    
+    token = create_access_token(user.username, user.id, user.role, timedelta(minutes=10))
+
+    response = JSONResponse(content={'access_token': token, 'token_type': 'bearer'})
+    return response
+
+@router.delete('/{user_id}',status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(db:db_dependency, user_id: int = Path(gt=0)):
+    user = db.query(Users).filter(Users.id==user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found.')
+    db.delete(user)
+    db.commit()
--- api/harrayApp/utils/security.py
+++ api/harrayApp/utils/security.py
@@ -1,3 +1,30 @@
+from datetime import timedelta, datetime, timezone
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from dotenv import load_dotenv
+import os
+
+from ..database.user_model import Users
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username==username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(username: str, user_id: int, role: str, expires_delta: timedelta):
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode = {'sub': username, 'id': user_id, 'role': role, 'exp': expires}
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
